7625c3b5d864cb61b8a112d8dd91b23d93776a6a
ilmovanbaarle <54939126+ilmovanbaarle@users.noreply.github.com>
2021-02-05 11:43:33 +0100

    Add files via upload

--- Creating_model_component.py
+++ Creating_model_component.py
@@ -17,13 +17,50 @@ from mesa.datacollection import DataCollector
 import networkx as nx
 import re
 
+def intersections(road):
+    df = pd.read_csv('_roads3.csv')
+    lijst = df['road'].unique()
+
+    matchers = ['N']
+    matching = [s for s in lijst if any(xs in s for xs in matchers)]
+
+    df2 = df[df['name'].str.contains('|'.join(matching))].reset_index().drop('index', axis=1)
+    side = df2[df2['road'].str.contains('|'.join(matching))].reset_index().drop('index', axis=1)
+
+    side = side.rename(columns={'chainage':'km'})
+    side['model_type'] = 'intersection'
+
+    side['intersection'] = ""
+    side['length'] = 4
+    side = side[side.road == road].reset_index().drop("index", axis=1)
+    n = 0
+
+    for i in side.name:
+        wordlist = re.sub("[^\w]", " ",  i).split()
+        for j in matching:
+            if any(j in s for s in wordlist):
+                match = [s for s in wordlist if j in s]
+                if len(match) == 2:
+                    side.loc[n,'intersection'] = str(match[0] + ', ' + match[1])
+                elif len(match) == 3:
+                    side.loc[n,'intersection'] = str(match[0] + match[1] + match[2])
+                elif len(match)==1:
+                    side.loc[n,'intersection'] = match[0]
+        n += 1
+
+    side = side[['road','km','length','lat','lon','model_type','intersection',]]
+        
+    return side
+
 def model_structure(road):
-    df= pd.read_csv('./data/CandV_Bridges.csv')
+    df= pd.read_csv('CandV_Bridges.csv')
+    df['model_type'] = 'bridge'
+    side = intersections(road)
+    df = pd.concat([df,side])
     df_road = df.loc[df.road==road,:].sort_values('km')
-    df_road = df_road[['km','length','condition','lat','lon']].reset_index().drop('index',axis=1)
-    df_road['model_type'] = 'bridge'
+    df_road = df_road[['km','length','condition','lat','lon','intersection','model_type']].reset_index().drop('index',axis=1)
     
-    df2 = pd.read_csv('./data/_roads3.csv')
+    df2 = pd.read_csv('_roads3.csv')
     df_start_end = df2.loc[df2.road==road,:]
     df_start_end = df_start_end.iloc[[0, -1]].reset_index().drop('index', axis=1)
     df_start_end = df_start_end.rename(columns={'chainage':'km'})
@@ -38,8 +75,8 @@ def model_structure(road):
     df_start_end.loc[1,'id'] = 100000000
     df_start_end.loc[1,'model_type'] = 'sink'
     
-    links = pd.DataFrame(columns=['km','length','condition','lat','lon'])
-
+    links = pd.DataFrame(columns=['km','length','condition','lat','lon','intersection'])
+    
     for i in range(len(df_road)):
         links = links.append(df_road.iloc[i,:])
         if i == 0:
@@ -50,9 +87,14 @@ def model_structure(road):
             links.loc[i,'km'] = df_road.loc[i,'km'] - df_road.loc[i-1,'km']
             links.loc[i,'lat'] = (df_road.loc[i,'lat'] + df_road.loc[i-1,'lat'])/2
             links.loc[i,'lon'] = (df_road.loc[i,'lon'] + df_road.loc[i-1,'lon'])/2
-        if i == len(df_road)-1:
+        if i == len(df_road)-1 and len(df_road) > 1:
             links = links.append(df_start_end.iloc[1,:]).reset_index().drop('index',axis=1)
             links.loc[i+1,'km'] = df_start_end.loc[1,'km'] - df_road.loc[i-1,'km']
+            links.loc[i+1,'lat'] = (df_road.loc[i,'lat'] + df_start_end.loc[1,'lat'])/2
+            links.loc[i+1,'lon'] = (df_road.loc[i,'lon'] + df_start_end.loc[1,'lon'])/2
+        if i == len(df_road)-1 and len(df_road) <= 1:
+            links = links.append(df_start_end.iloc[1,:]).reset_index().drop('index',axis=1)
+            links.loc[i+1,'km'] = df_start_end.loc[1,'km'] - df_road.loc[0,'km']
             links.loc[i+1,'lat'] = (df_road.loc[i,'lat'] + df_start_end.loc[1,'lat'])/2
             links.loc[i+1,'lon'] = (df_road.loc[i,'lon'] + df_start_end.loc[1,'lon'])/2
     
@@ -66,7 +108,14 @@ def model_structure(road):
     df_road.loc[len(df_road)-1,'id'] = 10000+len(df_road)-1
     df_road['road'] = road
     
-    return df_road
     
+    return df_road
+
 df_road = model_structure('N1')
 df_road.to_csv("./data/df_road.csv")
+
+# main_road = intersections('N1')
+
+# for i in main_road.intersection:
+  #  print(i)
+  #  a = model_structure(i)
